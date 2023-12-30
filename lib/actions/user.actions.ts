551d72d4475464d06707a75b2f413537f891a646
naweli777 <naweliverma7@gmail.com>
2023-12-30 17:06:08 +0530

    ts issue

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Order from "../database/models/order.model";
+import Event from "../database/models/event.model";
 import { revalidatePath } from "next/cache";
 
 
